25cd8a1d3ab77833247f95810b2273aae2929f30
Tiago Valdrich <tiago.valdrich@hotmail.com>
2019-01-25 20:54:20 -0200

    Creating the method findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,10 @@ package application;
 
 import model.DAO.DaoFactory;
 import model.DAO.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 public class Program {
 
@@ -11,10 +14,20 @@ public class Program {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: Seller findById() ===");
+
         Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+
         System.out.println("=== ------------------------- ===");
 
-        System.out.println(seller);
+        System.out.println("\n=== TEST 2: Seller findByDepartment() ===");
+
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        list.forEach(System.out::println);
+
+        System.out.println("=== ------------------------- ===");
 
     }
 
