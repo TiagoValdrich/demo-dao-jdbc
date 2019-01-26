5a5cb90970dcbfd05b5893eca3b02186cc5d7357
Tiago Valdrich <tiago.valdrich@hotmail.com>
2019-01-26 10:29:50 -0200

    Finishing the Test Class

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,6 +4,8 @@ import model.DAO.DaoFactory;
 import model.DAO.DepartmentDao;
 import model.entities.Department;
 
+import java.util.List;
+
 public class Program2 {
 
     public static void main(String[] args) {
@@ -17,6 +19,40 @@ public class Program2 {
         departmentDao.insert(department);
 
         System.out.println("Department inserted sucessfully, ID: " + department.getId());
+
+        System.out.println("=== ------------------------- ===");
+
+        System.out.println("=== TEST 2: Department update() ===");
+
+        department = new Department(1, "Books");
+
+        departmentDao.update(department);
+
+        System.out.println("Department updated!");
+
+        System.out.println("=== ------------------------- ===");
+
+        System.out.println("=== TEST 3: Department deleteById() ===");
+
+        departmentDao.deleteById(7);
+
+        System.out.println("Department deleted sucessfully");
+
+        System.out.println("=== ------------------------- ===");
+
+        System.out.println("=== TEST 4: Department findById() ===");
+
+        Department departmentFound = departmentDao.findById(4);
+
+        System.out.println(departmentFound);
+
+        System.out.println("=== ------------------------- ===");
+
+        System.out.println("=== TEST 5: Department findAll() ===");
+
+        List<Department> list = departmentDao.findAll();
+
+        list.forEach(System.out::println);
 
         System.out.println("=== ------------------------- ===");
 
